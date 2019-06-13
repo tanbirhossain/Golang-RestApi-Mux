57bfc073b5bdeb098d147fe0ba7b3c5c3df0b1b3
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-06-13 11:38:26 +0900

    update my sql database crud and

--- main.go
+++ main.go
@@ -3,90 +3,91 @@ package main
 import (
 	"encoding/json"
 	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-// Book struct (model)
-type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
+type Post struct {
+	Id      int    `json:id`
+	Title   string `json:title`
+	Content string `json:content`
 }
-
-type Author struct {
-	Firstname string `json:"firstname"`
-	LastName  string `json:"lastname"`
-}
-
-//Init books var as a slice Books structs
-var books []Book
 
 //Get all books
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
+
+	// json.NewEncoder(w).Encode(books)
+
+	// database connection
+	db := gormConnect()
+	defer db.Close()
+
+	posts := []Post{}
+	db.Find(&posts)
+	json.NewEncoder(w).Encode(posts)
 }
 
 //Get single Book
 func getBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) //Get params
+	// database connection
+	db := gormConnect()
+	defer db.Close()
 
-	for _, item := range books {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Book{}) // if nothing found here
+	post := Post{} // simple model
+	db.Where("id = ?", params["id"]).Find(&post)
+	// db.First(&post, "id= ?", params["id"])
+	json.NewEncoder(w).Encode(post)
+
 }
 
 //Create a new book
 func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(1000000)) // Mock ID - not safe
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
+
+	// database connection
+	db := gormConnect()
+	defer db.Close()
+	post := Post{}
+	// var book Book
+	_ = json.NewDecoder(r.Body).Decode(&post)
+	db.Save(&post)
+	json.NewEncoder(w).Encode(post)
+
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) //Get params
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = params["id"] // Mock ID - not safe
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
-			return
-		}
+	// database connection
+	db := gormConnect()
+	defer db.Close()
+	post := Post{}
+	db.First(&post, params["id"]) // get value from database
+	_ = json.NewDecoder(r.Body).Decode(&post)
+	db.Save(&post)
+	json.NewEncoder(w).Encode(post)
 
-	}
-	json.NewEncoder(w).Encode(books)
 }
 func deleteBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) //Get params
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			break
-		}
 
-	}
-	json.NewEncoder(w).Encode(books)
+	// database connection
+	db := gormConnect()
+	defer db.Close()
+	post := Post{}
+	db.First(&post, params["id"]) // get value from database
+	db.Delete(&post)
+	json.NewEncoder(w).Encode(post)
 }
+
+//db connection
 func gormConnect() *gorm.DB {
 	DBMS := "mysql"
 	USER := "root"
@@ -102,19 +103,10 @@ func gormConnect() *gorm.DB {
 	}
 	return db
 }
-func main() {
 
-	// database connection
-	db := gormConnect()
-	defer db.Close()
-
+func routing() {
 	//int Router
 	r := mux.NewRouter()
-
-	//Mock data - @todo - implemet DB
-	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One", Author: &Author{Firstname: "John", LastName: "Doe"}})
-	books = append(books, Book{ID: "2", Isbn: "44822743", Title: "Book two", Author: &Author{Firstname: "John", LastName: "kasem"}})
-
 	//Router Handler / Endpoints chill
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
 	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
@@ -124,4 +116,13 @@ func main() {
 
 	//listner
 	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func main() {
+
+	// database connection
+	db := gormConnect()
+	defer db.Close()
+	//declare all routing
+	routing()
 }
